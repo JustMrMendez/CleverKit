8acd4753353e3c5f6b82c91d8311627ff2bb1f0f
JustMrMendez <mrmendez@amvestor.com>
2023-03-05 13:40:57 -0500

    added simple text logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Logo from './Logo.svelte';
+
 	import '../theme.postcss';
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
@@ -6,9 +8,19 @@
 	import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
-<AppBar padding="p-4 fixed w-full" gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-	<svelte:fragment slot="lead">(icon)</svelte:fragment>
-	(title)
+<AppBar
+	padding="p-4 fixed w-full"
+	gridColumns="grid-cols-3"
+	slotDefault="place-self-center"
+	slotTrail="place-content-end"
+>
+	<svelte:fragment slot="lead">
+		<Logo />
+	</svelte:fragment>
+	<ul class="list-nav flex">
+		<li><a href="/login">Login</a></li>
+		<li><a href="/signup">Signup</a></li>
+	</ul>
 	<svelte:fragment slot="trail">
 		<LightSwitch height="h-6" />
 	</svelte:fragment>
---
+++ src/routes/Logo.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../theme.postcss';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+
+	import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+		<a href="/" class="group text-xl text-primary-600-300-token font-bold text-center [&>*]:transition-all [&>*]:duration-300">
+			<span class="group-hover:bg-surface-500/25 p-1 rounded-l-sm">Clever</span><span
+			class="p-1 rounded-r text-error-600-300-token group-hover:bg-primary-500/25">Kit</span
+			>
+		</a>
+
